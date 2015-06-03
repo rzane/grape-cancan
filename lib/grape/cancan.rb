cb72ba13191434f391429a0baff1ff3db8da2b60
Ray Zane <rzane@bodnargroup.com>
2015-06-03 16:58:25 -0400

    Get rid of some files. Rename modules

--- lib/grape/cancan.rb
+++ lib/grape/cancan.rb
@@ -3,10 +3,28 @@ require 'grape/cancan/version'
 
 module Grape
   module CanCan
-    autoload :Helpers,    'grape/cancan/helpers'
-    autoload :Authorizer, 'grape/cancan/authorizer'
+    module API
+      # Convienience method to authorize every route
+      def authorize_routes!
+        before { authorize_route! }
+      end
+    end
 
-    Grape::Endpoint.send :include, Grape::CanCan::Helpers
-    Grape::API.extend Grape::CanCan::Authorizer
+    module Endpoint
+      # Returns an instance of the CanCan ability
+      def current_ability
+        @current_ability ||= ::Ability.new(current_user)
+      end
+      delegate :can?, :cannot?, :authorize!, to: :current_ability
+
+      # Call authorize using the :authorize key on the route
+      def authorize_route!
+        opts = env['api.endpoint'].options[:route_options]
+        authorize!(*opts[:authorize]) if opts.key?(:authorize)
+      end
+    end
+
+    Grape::API.extend Grape::CanCan::API
+    Grape::Endpoint.send :include, Grape::CanCan::Endpoint
   end
 end
